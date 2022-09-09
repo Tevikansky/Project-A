32175b38216ab5c888434b0bcfc382ee0bf3b4a5
Tevikansky <valerabogdanov@inbox.ru>
2022-09-09 14:16:10 +0600

    fix: js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,37 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu");
 const menu = document.querySelector(".mobile-menu-main");
+
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo-dark";
+};
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo";
+};
+
+const openMenu = (event) => {
+  // функцияя открывания мею
+  menu.classList.add("is-open");
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow = "hidden";
+  lightModeOn();
+
+};
+const closeMenu = (event) => {
+  // функцияя закрывания мею
+  menu.classList.remove("is-open");
+  mMenuToggle.classList.remove("close-menu");
+  document.body.style.overflow = "";
+  this.scrollY < 1 ? lightModeOff() : lightModeOn();
+};
+
 window.addEventListener("scroll", () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo-dark";
-  } else {
-    console.log('Мы наверху')
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo";
-  }
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
 });
+
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.toggle("is-open");
-})
+  menu.classList.contains('is-open') ? closeMenu() : openMenu();
+});
