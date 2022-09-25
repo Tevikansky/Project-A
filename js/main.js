7f81af36f9e50ba56dda0799641dabee7375e836
Tevikansky <valerabogdanov@inbox.ru>
2022-09-25 18:03:04 +0600

    fix: button-close

--- js/main.js
+++ js/main.js
@@ -117,3 +117,23 @@ const swiperBlog = new Swiper('.swiper-blog', {
     }
   }
 });
+
+const modal = document.querySelector(".modal");
+const modalDialog = document.querySelector(".modal-dialog");
+
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNote.dataset.toggle == "modal" ||
+    !event.composedPath().includes(modalDialog) && modal.classList.contains("is-open")
+  ) {
+    event.preventDefault();
+    modal.classList.toggle("is-open");
+  }
+});
+document.addEventListener('keyup', (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
+  }
+})
